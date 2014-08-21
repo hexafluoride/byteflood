b70ce14f3b80b0e0736b8219f165df41daab2da3
diantahoc <omar.zanhour@outlook.com>
2014-08-21 07:25:57 +0300

    Added PieceView mode.

---
+++ ByteFlood/Formatters/PieceViewConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ByteFlood.Formatters
+{
+    public class PieceViewConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (System.Convert.ToBoolean(value))
+            {
+                return Brushes.Red;
+            }
+            else
+            {
+                return Brushes.Black;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
