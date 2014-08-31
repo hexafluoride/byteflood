5e3c1d59ad6e1f9e7965b99be6be003a83f885be
diantahoc <omar.zanhour@outlook.com>
2014-08-31 07:47:12 +0300

    Implement a treeview for files.

--- ByteFlood/InfoClasses/FileInfo.cs
+++ ByteFlood/InfoClasses/FileInfo.cs
@@ -10,35 +10,61 @@ namespace ByteFlood
 {
     public class FileInfo : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-        public string Name { get; set; }
-        public int Progress { get; set; }
-        public string Priority { get { return ActualPriority == MonoTorrent.Common.Priority.DoNotDownload ? "Don't download" : ActualPriority.ToString(); } }
-        public Priority ActualPriority { get; set; }
-        public bool DownloadFile { get; set; }
-
-        public string Size { get { return Utility.PrettifyAmount(RawSize); } }
-
-        public long RawSize { get; set; }
-        [XmlIgnore]
-        public TorrentInfo Parent { get; set; }
-        public FileInfo()
+        public string Name
         {
+            get
+            {
+                return App.Settings.ShowRelativePaths ? this.File.Path : this.File.FullPath;
+            }
         }
 
-        public FileInfo(TorrentInfo owner) 
+        public double Progress { get { return this.File.BitField.PercentComplete; } }
+
+        public string Priority
         {
-            Parent = owner;
+            get { return this.File.Priority.ToString(); }
         }
 
-        public void SetSelf(FileInfo pi)
+        public void ChangePriority(Priority pr)
         {
-            this.Name = pi.Name;
-            this.Progress = pi.Progress;
-            this.ActualPriority = pi.ActualPriority;
-            this.DownloadFile = pi.DownloadFile;
-            UpdateList("Name", "Progress", "Priority", "DownloadFile");
+            if (this.File.Priority != pr)
+            {
+                this.File.Priority = pr;
+                UpdateList("Priority");
+            }
         }
+
+        public bool DownloadFile
+        {
+            get { return this.File.Priority != MonoTorrent.Common.Priority.Skip; }
+            set
+            {
+                if (value)
+                {
+                    this.File.Priority = MonoTorrent.Common.Priority.Normal;
+                }
+                else
+                {
+                    this.File.Priority = MonoTorrent.Common.Priority.Skip;
+                }
+            }
+        }
+
+        public string Size { get { return Utility.PrettifyAmount(this.File.Length); } }
+
+        public TorrentFile File { get; private set; }
+
+        public long RawSize { get { return this.File.Length; } }
+
+        public FileInfo() { }
+
+        public FileInfo(TorrentInfo owner, TorrentFile file)
+        {
+            this.File = file;
+        }
+
+        #region INotifyPropertyChanged implementation
+
         public void UpdateList(params string[] columns)
         {
             if (PropertyChanged == null)
@@ -46,5 +72,83 @@ namespace ByteFlood
             foreach (string str in columns)
                 PropertyChanged(this, new PropertyChangedEventArgs(str));
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
     }
+
+    public class DirectoryKey : System.Collections.Hashtable
+    {
+        //public const string FILE_MARKER = "<files>";
+
+        public DirectoryKey(string name)
+        {
+            //base.Add(FILE_MARKER, new FileList());
+            this.Name = name;
+        }
+
+
+        public string Name { get; private set; }
+
+        //public List<FileInfo> Files 
+        //{
+        //    get 
+        //    {
+        //        return (List<FileInfo>)base[FILE_MARKER];
+        //    }
+        //}
+
+        /// <summary>
+        /// No use outside of TorrentInfo.PopulateFileList()
+        /// </summary>
+        /// <param name="branch"></param>
+        /// <param name="trunk"></param>
+        public static void ProcessFile(string branch, DirectoryKey trunk, TorrentInfo owner, TorrentFile f)
+        {
+            string[] parts = branch.Split('\\');
+            if (parts.Length == 1)
+            {
+                //((FileList)trunk[DirectoryKey.FILE_MARKER]).Add(new FileInfo(owner, f));
+                trunk.Add(f.FullPath, new FileInfo(owner, f));
+            }
+            else
+            {
+                string node = parts[0];
+                string other = branch.Substring(node.Length + 1);
+
+                if (!trunk.ContainsKey(node))
+                {
+                    trunk[node] = new DirectoryKey(node);
+                }
+                ProcessFile(other, (DirectoryKey)trunk[node], owner, f);
+            }
+        }
+
+
+    }
+
+    //public class FileList 
+    //{
+    //    private List<FileInfo> a = new List<FileInfo>();
+
+    //    public void Add(FileInfo f) { a.Add(f); }
+
+    //    public FileInfo this[int index] 
+    //    {
+    //        get 
+    //        {
+    //            return a[index];
+    //        }
+    //        set 
+    //        {
+    //            a[index] = value;
+    //        }
+    //    }
+
+    //    public FileInfo[] Files 
+    //    {
+    //        get { return a.ToArray(); }
+    //    }
+    //}
 }
