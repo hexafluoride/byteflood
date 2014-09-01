7b1d4e9bdea835414e6454fa1a70375a620470d6
hexafluoride <xafluoride@gmail.com>
2014-09-02 00:45:53 +0300

    Actually do what the other commit meant to do and revert some changes

--- ByteFlood/UI/MainWindow.xaml.cs
+++ ByteFlood/UI/MainWindow.xaml.cs
@@ -674,7 +674,8 @@ namespace ByteFlood
                 case WindowBehavior.MinimizeToTray:
                     this.WindowState = System.Windows.WindowState.Minimized;
                     this.ShowInTaskbar = false;
-                    NotifyIcon.ShowBalloonTip("ByteFlood", "ByteFlood has been minimized to the traybar.", Hardcodet.Wpf.TaskbarNotification.BalloonIcon.Info);
+                    if(App.Settings.NotifyOnTray)
+                        NotifyIcon.ShowBalloonTip("ByteFlood", "ByteFlood has been minimized to the traybar.", Hardcodet.Wpf.TaskbarNotification.BalloonIcon.Info);
                     break;
                 case WindowBehavior.Exit:
                     ignoreclose = false;
