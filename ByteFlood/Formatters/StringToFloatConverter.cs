1f8cbf8a908bc8a4aedb3cbe6ab9c57a0f994dfb
diantahoc <omar.zanhour@outlook.com>
2014-09-05 02:01:19 +0300

    Remove redundant code in the AddTorrentDialog and re-do it with DependencyProperties and UICommands, while removing uncessary element names.
    Because why not.

---
+++ ByteFlood/Formatters/StringToFloatConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace ByteFlood.Formatters
+{
+    public class StringToFloatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //value is float, we need a string (float is ok too)
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string s = System.Convert.ToString(value);
+
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0f;
+            }
+            else
+            {
+                try
+                {
+                    s = s.Replace(',', '.');
+                    float vae = float.Parse(s);
+                    if (vae < 0)
+                    {
+                        vae = 0;
+                    }
+                    return vae;
+                }
+                catch 
+                {
+                    return "invalid"; // trigget the data error
+                }
+            }
+        }
+    }
+}
