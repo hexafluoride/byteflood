f09fa5e42642e5c24fe17b8183ce6219bd10e7fe
diantahoc <omar.zanhour@outlook.com>
2014-09-23 22:40:09 +0300

    Improvements.
    - Stability improvements.
    - Asynchronous loading of some images.

---
+++ ByteFlood/Controls/PhotoLoader/ImageLoaders/ExternalLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace PhotoLoader.ImageLoaders
+{
+    class ExternalLoader : ILoader
+    {
+        #region ILoader Members
+
+        public System.IO.Stream Load(string source)
+        {
+            using (var webClient = new WebClient())
+            {
+                try
+                {
+                    byte[] html = webClient.DownloadData(source);
+
+                    if (html == null || html.Length == 0) return null;
+
+                    return new MemoryStream(html);
+                }
+                catch { return null; }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ ByteFlood/Controls/PhotoLoader/Types.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhotoLoader
+{
+    public enum SourceType
+    {
+        LocalDisk,
+        ExternalResource,
+        CachedExternalResoucre
+    }
+
+    public enum DisplayOptions
+    {
+        Preview,
+        FullResolution
+    }
+}
