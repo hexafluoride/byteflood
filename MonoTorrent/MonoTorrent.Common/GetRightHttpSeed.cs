166c3751db67e5a6b6a7f44d6fe06c4b5c80838c
diantahoc <omar.zanhour@outlook.com>
2014-10-10 01:19:12 +0300

    Improve webseeds handling.

---
+++ MonoTorrent/MonoTorrent.Common/GetRightHttpSeed.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonoTorrent.Common
+{
+    public class GetRightHttpSeed
+    {
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Indicate if this webseed has been added to the peer list and is being used,
+        /// so we don't add it twice.
+        /// </summary>
+        public bool HasBeenAdded { get; set; }
+
+        public GetRightHttpSeed() 
+        {
+            this.HasBeenAdded = false;
+        }
+    }
+}
