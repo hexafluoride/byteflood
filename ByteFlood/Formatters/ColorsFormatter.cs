5300d5eaeaaf7118d8f196e2b16060d4d37008b6
diantahoc <omar.zanhour@outlook.com>
2014-11-04 20:48:05 +0200

    Add vector icons, and Windows Aero color integration.

---
+++ ByteFlood/Formatters/ColorsFormatter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ByteFlood.Formatters
+{
+    public class ForegroundColorProvider : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Color c = (Color)value;
+            double luminance = 0.2126 * c.ScR + 0.7152 * c.ScG + 0.0722 * c.ScB;
+            if (luminance < 0.5)
+            {
+                return Brushes.White;
+            }
+            else
+            {
+                return Brushes.Black;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
